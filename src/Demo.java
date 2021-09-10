7b3c6c59383e9ea32c147ff34e7534161724a16b
Superconcinnity <chengwei2061@gmail.com>
2021-09-10 11:50:26 +0800

    - doc: :package: uploaded the homework

---
+++ src/Demo.java
@@ -0,0 +1,31 @@
+public class Demo {
+
+    public static void main(String[] args) {
+        // 声明 declaration
+        int v; // 整数
+        double d;//小数点
+        float  f;//浮点数
+        long l;//长整数
+
+        boolean b;//布尔值
+
+        char c;//字符
+
+        String s;//字符串
+
+        //  赋值 assign
+        v = 15;
+        b = false;
+        c = 'b';
+        s = "猫猫";
+
+        //输出 output
+        //输出15
+        System.out.println(v);
+        //输出30
+        v = 30;
+        System.out.println(v);
+        System.out.println(b);
+    }
+
+}
---
+++ src/Exercise_1.java
@@ -0,0 +1,75 @@
+public class Exercise_1 {
+
+    public static int age_rui = 1;
+
+    public static void main(String[] args) {
+//        int n = 10;
+//        int b = 3;
+//        System.out.println(n - b);
+//        System.out.println(n * b);
+//        System.out.println(n + b);
+//
+//        System.out.println(n / b);
+//        /// 余数
+//        System.out.println(n % b);
+//          Homework_1(5, 6);
+//          Homework_2("wahelioccc2061", "333");
+        Homework_3(1, 2, 3);
+    }
+
+
+    private static void Homework_2(String old_psw, String nw_psw) {
+        String initPsw = "wahelioccc2061";
+        /**
+         * 第一种
+         * 旧密码错误
+         *
+         * 第二种
+         * 旧密码正确 新密码与旧密码相同
+         *
+         * 第三种
+         * 旧密码正确 新密码为新 修改成功
+         * */
+        // TODO 修改密码
+        if(old_psw.equals(initPsw)) {
+            // 旧密码与数据库密码相匹配
+            // 进行密码修改
+            System.out.println("修改密码");
+
+            //TODO 判断新密码是否与旧密码相同
+            if (nw_psw.equals(initPsw)){
+                // 相同
+                System.out.println("新密码不能与旧密码相同！");
+            } else {
+                initPsw = nw_psw;
+                System.out.println("密码修改成功！已改为: " + initPsw);
+            }
+
+        } else {
+            System.out.println("密码输入错误");
+        }
+    }
+
+    private static void Homework_1(int m, int n) {
+       /**
+        * 1. n / 10 + n % 20
+        * 2. n % 2 + m % 20
+        * 3. (m + n) / 2
+        * 4. (m + n) / 2.0
+        * 5. (int) (0.5 * (m + n))
+        * 6. m * m + n / 4 + 2 * n
+       * */
+       //TODO 输出上述五个运算结果
+        System.out.println(n / 10 + n % 20);
+        System.out.println(n % 2 + m % 20);
+        System.out.println( (m + n) / 2);
+        System.out.println( (m + n) / 2.0);
+        System.out.println((int) (0.3 * (m + n)));
+    }
+
+    private static void Homework_3(int a, int b, int c){
+        //TODO 输出三个数中最大的数字
+
+    }
+
+}
